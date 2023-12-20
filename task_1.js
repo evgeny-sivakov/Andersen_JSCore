6a9601843d6b70601b4c60a25f59654d7538d361
Evgeny Sivakov <jsivakov@gmail.com>
2023-12-20 16:39:59 +0100

    Andersen_JSCore/task_1

---
+++ task_1.js
@@ -0,0 +1,37 @@
+// Converts input to a defined numeric system.
+
+function convertNumber() {
+  const sourceNumber = +window.prompt('Enter number to convert');
+  const radix = +window.prompt('Enter numeral system base');
+
+  if (isNaN(sourceNumber) || isNaN(radix)) {
+    console.log('Некорректный ввод!');
+    return;
+  }
+
+  const result = sourceNumber.toString(radix);
+
+  console.log(result);
+}
+
+// Checks the inputs for a valid integer and performs arithmetic operations.
+
+function addDivide() {
+  const first = +window.prompt('Enter the first number');
+
+  if (isNaN(first)) {
+    console.log('Некорректный ввод!');
+    return;
+  }
+
+  const second = +window.prompt('Enter the second number');
+
+  if (isNaN(second)) {
+    console.log('Некорректный ввод!');
+    return;
+  }
+
+  const result = `Ответ: ${first + second}, ${first / second}`;
+
+  console.log(result);
+}
